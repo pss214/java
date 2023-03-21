eee4c449fd9ff76178cd784245a3eddb5e4ec6ed
박성수 <bak10175@gmail.com>
2023-03-21 15:22:07 +0900

    0321

---
+++ first/src/java0321/ContinuBreak.java
@@ -0,0 +1,16 @@
+package java0321;
+
+public class ContinuBreak {
+    public static void main(String[] args) {
+        int total = 0;
+        int sum = 0;
+
+        for (int num = 1; num <= 100;num++ ) {
+            if(num % 2 == 0){
+                continue;
+            }
+            total += num;
+        }
+        System.out.println("1부터 100까지 홀수의 합은 "+total+"입니다");
+    }
+}
